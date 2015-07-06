370b9cae110c0a2d44df611e69ddea063d7a51cd
Pavel Elagin <elagin.pasha@gmail.com>
2015-07-07 00:29:07 +0300

    Добавлены типы ТС и активность инспектора.

--- app/src/main/java/com/mototime/motobat/NewPointActivity.java
+++ app/src/main/java/com/mototime/motobat/NewPointActivity.java
@@ -1,17 +1,42 @@
 package com.mototime.motobat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 
-public class NewPointActivity extends Activity {
+public class NewPointActivity extends Activity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+
+    Spinner manAcvititySpinner;
+    Spinner vehicleTypeSpinner;
+    Button createBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_point);
+
+        createBtn = (Button) findViewById(R.id.createBtn);
+        createBtn.setOnClickListener(this);
+
+        manAcvititySpinner = (Spinner) findViewById(R.id.manAcvititySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.man_acvitity_spinner_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        manAcvititySpinner.setAdapter(adapter);
+        manAcvititySpinner.setOnItemSelectedListener(this);
+
+        vehicleTypeSpinner = (Spinner) findViewById(R.id.vehicleTypeSpinner);
+        ArrayAdapter<CharSequence> vehicleTypeAdapter = ArrayAdapter.createFromResource(this, R.array.man_vehicle_spinner_array, android.R.layout.simple_spinner_item);
+        vehicleTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        vehicleTypeSpinner.setAdapter(vehicleTypeAdapter);
+        vehicleTypeSpinner.setOnItemSelectedListener(this);
     }
 
     @Override
@@ -34,5 +59,24 @@ public class NewPointActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+//        String aaa = parent.getSelectedItem().toString();
+        createBtn.setEnabled(manAcvititySpinner.getSelectedItemPosition() != 0 && vehicleTypeSpinner.getSelectedItemPosition() != 0);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.createBtn:
+                finish();
+                break;
+        }
     }
 }
