3a1b1b4cd1af9673d96c1f7fd78ef6a4b8eaf30d
Pavel Elagin <elagin.pasha@gmail.com>
2015-07-06 23:31:43 +0300

    Добавлено активити для создания новой точки.

--- app/src/main/java/com/mototime/motobat/NewPointActivity.java
+++ app/src/main/java/com/mototime/motobat/NewPointActivity.java
@@ -1,31 +1,23 @@
 package com.mototime.motobat;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity implements View.OnClickListener  {
-
-    private Button newPointBtn;
+public class NewPointActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        newPointBtn = (Button)findViewById(R.id.new_point_btn);
-        newPointBtn.setOnClickListener(this);
+        setContentView(R.layout.activity_new_point);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_new_point, menu);
         return true;
     }
 
@@ -42,15 +34,5 @@ public class MainActivity extends Activity implements View.OnClickListener  {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        switch (id) {
-            case R.id.new_point_btn:
-                startActivity(new Intent(this, NewPointActivity.class));
-                break;
-        }
     }
 }
