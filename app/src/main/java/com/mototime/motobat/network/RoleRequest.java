a190d682e5dc6588cbb1681fc5a3aa063875c7c9
rjhdby <andrewgrom@rambler.ru>
2015-07-11 11:12:49 +0300

    Переработка network

--- app/src/main/java/com/mototime/motobat/network/RoleRequest.java
+++ app/src/main/java/com/mototime/motobat/network/RoleRequest.java
@@ -13,8 +13,6 @@ import java.util.HashMap;
 public class RoleRequest extends HTTPClient  {
 
     private final String userID;
-    private final String VALID_RESULT = "RESULT";
-    private final String INVALID_RESULT = "ERROR";
 
     public RoleRequest(AsyncTaskCompleteListener listener, Context context, String userID) {
         this.listener = listener;
@@ -24,30 +22,6 @@ public class RoleRequest extends HTTPClient  {
         post = new HashMap<>();
         post.put("method", "getRole");
         post.put("userid", userID);
-        post.put("userid", "rjhdby");
         execute(post);
-    }
-
-    @Override
-    public boolean error(JSONObject response) {
-        return response.has(INVALID_RESULT);
-    }
-
-    @Override
-    public String getError(JSONObject response) {
-        if (!response.has(VALID_RESULT))
-            return "Ошибка соединения "  + response.toString();
-        try {
-            String result = response.getString("result");
-            switch (result) {
-                case "OK":
-                    return "Статус изменен";
-                case "ERROR PREREQUISITES":
-                    return "Неизвестная ошибка " + response.toString();
-            }
-        } catch (JSONException ignored) {
-
-        }
-        return "Неизвестная ошибка " + response.toString();
     }
 }
