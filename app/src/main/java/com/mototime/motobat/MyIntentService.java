81f7cb0d8dbc15f3f699433204a306fbd7e90a07
Pavel Elagin <elagin.pasha@gmail.com>
2015-10-09 00:49:27 +0300

    Убираем повтороное использование кода. Строки переносим в константы.

--- app/src/main/java/com/mototime/motobat/MyIntentService.java
+++ app/src/main/java/com/mototime/motobat/MyIntentService.java
@@ -34,7 +34,8 @@ public class MyIntentService extends IntentService {
 
     public static final String ERROR = "error";
 
-
+    public static final String RECEIVER_TAG = "receiverTag";
+    public static final String ACTION = "action";
     public static final String ACTION_GET_POINT_LIST = "com.mototime.motobat.action.GetPointList";
     public static final String ACTION_CREATE_POINT = "com.mototime.motobat.action.CreatePoint";
     public static final String ACTION_GET_ROLE = "com.mototime.motobat.action.GetRole";
@@ -43,34 +44,38 @@ public class MyIntentService extends IntentService {
     public static final String USER_NAME = "userName";
     public static final String VERSION_NAME = "versionName";
 
+    public static final String POINT = "point";
+    public static final String MEMBER_GROUP = "memberGroup";
+
     public static final String RESULT = "RESULT";
 
     //private BroadcastNotifier mBroadcaster = new BroadcastNotifier(this);
 
+    private static Intent newIntent(Context context, String action, MyResultReceiver receiver) {
+        Intent res = new Intent(context, MyIntentService.class);
+        res.setAction(action);
+        res.putExtra(RECEIVER_TAG, receiver);
+        return res;
+    }
 
     public static void startActionCreatePoint(Context context, MyResultReceiver receiver, NewPointSerializable point, String memberGroup) {
-        Intent intent = new Intent(context, MyIntentService.class);
-        intent.setAction(ACTION_CREATE_POINT);
-        intent.putExtra("receiverTag", receiver);
-
-        intent.putExtra("point", point);
-        intent.putExtra("memberGroup", memberGroup);
+        Intent intent = newIntent(context, ACTION_CREATE_POINT, receiver);
+        intent.putExtra(POINT, point);
+        intent.putExtra(MEMBER_GROUP, memberGroup);
         context.startService(intent);
     }
 
     public static void startActionGetPointList(Context context, MyResultReceiver receiver) {
-        Intent intent = new Intent(context, MyIntentService.class);
-
-        intent.setAction(ACTION_GET_POINT_LIST);
-        intent.putExtra("receiverTag", receiver);
-
+        Intent intent = newIntent(context, ACTION_GET_POINT_LIST, receiver);
+//        intent.setAction(ACTION_GET_POINT_LIST);
+//        intent.putExtra(RECEIVER_TAG, receiver);
         context.startService(intent);
     }
 
     public static void startActionGetRole(Context context, MyResultReceiver receiver, String userID, String userName, String versionName) {
-        Intent intent = new Intent(context, MyIntentService.class);
-        intent.setAction(ACTION_GET_ROLE);
-        intent.putExtra("receiverTag", receiver);
+        Intent intent = newIntent(context, ACTION_GET_ROLE, receiver);
+//        intent.setAction(ACTION_GET_ROLE);
+//        intent.putExtra(RECEIVER_TAG, receiver);
 
         intent.putExtra(USER_ID, userID);
         intent.putExtra(USER_NAME, userName);
@@ -84,16 +89,16 @@ public class MyIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        ResultReceiver rec = intent.getParcelableExtra("receiverTag");
+        ResultReceiver rec = intent.getParcelableExtra(RECEIVER_TAG);
         //String recName= intent.getStringExtra("nameTag");
         final String action = intent.getAction();
         Bundle bundle = new Bundle();
-        bundle.putString("action", action);
+        bundle.putString(ACTION, action);
 
         if (ACTION_CREATE_POINT.equals(action)) {
-            final NewPointSerializable newPointSerializable = (NewPointSerializable) intent.getSerializableExtra("point");
-            final String memberGroup = intent.getStringExtra("memberGroup");
-            JSONObject response = handleActionCreatePoint(newPointSerializable, memberGroup);
+//            final NewPointSerializable newPointSerializable = (NewPointSerializable) intent.getSerializableExtra("point");
+//            final String memberGroup = intent.getStringExtra("memberGroup");
+            JSONObject response = handleActionCreatePoint(intent);
             rec.send(MyResultReceiver.SUCCSESS_RESULT, bundle);
         } else if (ACTION_GET_POINT_LIST.equals(action)) {
             JSONObject response = handleActionGetPointList();
@@ -111,10 +116,10 @@ public class MyIntentService extends IntentService {
                 rec.send(MyResultReceiver.SUCCSESS_RESULT, bundle);
             }
         } else if (ACTION_GET_ROLE.equals(action)) {
-            final String userID = intent.getStringExtra(USER_ID);
-            final String userName = intent.getStringExtra(USER_NAME);
-            final String versionName = intent.getStringExtra(VERSION_NAME);
-            JSONObject response = handleRoleRequest(userID, userName, versionName);
+//            final String userID = intent.getStringExtra(USER_ID);
+//            final String userName = intent.getStringExtra(USER_NAME);
+//            final String versionName = intent.getStringExtra(VERSION_NAME);
+            JSONObject response = handleRoleRequest(intent);
             if (RequestErrors.isError(response)) {
                 bundle.putString(ERROR, RequestErrors.getError(response));
                 rec.send(MyResultReceiver.ERROR_RESULT, bundle);
@@ -139,7 +144,10 @@ public class MyIntentService extends IntentService {
         }
     }
 
-    private JSONObject handleActionCreatePoint(NewPointSerializable point, String group) {
+    private JSONObject handleActionCreatePoint(Intent intent/*NewPointSerializable point, String group*/) {
+        final NewPointSerializable point = (NewPointSerializable) intent.getSerializableExtra(POINT);
+        final String group = intent.getStringExtra(MEMBER_GROUP);
+
         JSONObject result = new CreatePointRequestNew(this, point, group).request(myApp.getPreferences().getServerURI());
         return result;
     }
@@ -149,7 +157,10 @@ public class MyIntentService extends IntentService {
         return result;
     }
 
-    private JSONObject handleRoleRequest(String userID, String userName, String versionName) {
+    private JSONObject handleRoleRequest(Intent intent/*String userID, String userName, String versionName*/) {
+        final String userID = intent.getStringExtra(USER_ID);
+        final String userName = intent.getStringExtra(USER_NAME);
+        final String versionName = intent.getStringExtra(VERSION_NAME);
         return new RoleRequestNew(this, userID, userName, versionName).request(myApp.getPreferences().getServerURI());
     }
 
